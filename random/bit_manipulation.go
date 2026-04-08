792c0f2b977e7336a349d4cee7ab3c5579b59cfd
Leri <ed.nazarov001@gmail.com>
2026-04-08 19:21:05 +0300

    add(random): bit_manipulation case

---
+++ random/bit_manipulation.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+const (
+	FlagA byte = 1 << 0 // 00000001
+	FlagB byte = 1 << 1 // 00000010
+)
+
+func main() {
+	var status byte
+
+	fmt.Printf("Начальный status: %08b (%d)\n", status, status) // 00000000 (0)
+
+	status |= FlagA
+	fmt.Printf("После |= FlagA:  %08b (%d)\n", status, status) // 00000001 (1)
+
+	status &^= FlagA
+	fmt.Printf("После &^= FlagA: %08b (%d)\n", status, status) // 00000000 (0)
+
+	status |= FlagB
+	fmt.Printf("После |= FlagB:  %08b (%d)\n", status, status) // 00000010 (2)
+
+	status &^= FlagB
+	fmt.Printf("После &^= FlagB: %08b (%d)\n", status, status) // 00000000 (0)
+
+	if status&FlagA != 0 {
+		fmt.Println("FlagA включён")
+	}
+}
